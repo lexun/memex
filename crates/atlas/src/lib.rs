6b242f6c7b6705b87b8d4ec42917ee01b5f9fd53
Luke Barbuto <luke.barbuto@gmail.com>
2025-12-24 19:17:51 -1000

    Move daemon/config to cli crate, atlas is library

---
+++ crates/atlas/src/lib.rs
@@ -0,0 +1,8 @@
+//! Atlas - Memory and knowledge graph for Memex
+//!
+//! Atlas maps and navigates information terrain. It ingests episodes,
+//! builds graph representations, and generates context on demand.
+
+// TODO: Episode ingestion
+// TODO: Graph storage (SurrealDB)
+// TODO: Context generation
